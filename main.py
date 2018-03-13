f1a8282776c24e99ed2b31796dab638ed2ba8d03
Luca Mannella <luca.mannella@studenti.polito.it>
2018-03-13 23:49:02 +0100

    Aggiunta possibilità di configurare probabilità iniziale di 0/1, probabilità di mutazione e steady state

--- main.py
+++ main.py
@@ -23,7 +23,12 @@ def main():
     loci = list()
     genome_len = 1000  # h*w
     for _ in range(genome_len):
-        loci.append(sgx.LocusEnum([0, 1]))
+        if args.init_prob is not None:
+            init_prob_dict = {0: args.init_prob}
+        else:
+            init_prob_dict = None
+
+        loci.append(sgx.LocusEnum([0, 1], init_probs=init_prob_dict))
 
     genome = sgx.AlleleDistribution(loci)
     logging.debug("New Allele Distribution: %s", genome)
@@ -31,8 +36,12 @@ def main():
     opt = sgx.VanillaOptimizer(genome, fitness_function,
                                max_fitness=sgx.FitnessLexicographic(1.),
                                archive_max_size=5, fitness_log=LOG_NAME,
-                               mutation_probability=None)
-    opt.evolve()
+                               mutation_probability=args.mut_prob)
+    # per aggiungere condizioni di terminazione
+    # opt._stopping_conditions.append(lambda: _steady_state_function(opt, args.steady_state))
+
+    opt.evolve()  # esecuzione dell'evolutivo
+
     if len(opt.archive) is 0:
         logging.error("Archivio vuoto! Non c'è soluzione!")
     else:
@@ -44,6 +53,18 @@ def fitness_function(genome):
     # OBIETTIVO -> massimizzare il numero di celle a 1
     # return tuple(sum(genome)/len(genome))
     return sgx.FitnessLexicographic(sum(genome) / len(genome))
+
+
+def _steady_state_function(optimizer, max_gen_without_improvement):
+    to_stop = False
+
+    for i in optimizer.archive:
+        if (optimizer.generation - i.birth) > max_gen_without_improvement:
+            to_stop = True
+            logging.warning("Steady state reached! - Generation %d", optimizer.generation)
+            break
+
+    return to_stop
 
 
 if __name__ == "__main__":
@@ -60,6 +81,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Hungry Team Optimization process')
     parser.add_argument('-d', '--debug', action='store_true', help='Debug option')
+    parser.add_argument('-ip', '--init-prob', dest='init_prob', default=None, type=float,
+                        help='Initial probability of having 0 values in the chromosome - rate [0.0-1.0]')
+    parser.add_argument('-mp', '--mut-prob', dest='mut_prob', default=None, type=float,
+                        help='Mutation probability rate [0.0-1.0]')
+    parser.add_argument('-ss', '--steady-state', dest='steady_state', default=4000, type=int,
+                        help="The maximum number of generation explored without improvements")
     args = parser.parse_args()
 
     if args.debug:
