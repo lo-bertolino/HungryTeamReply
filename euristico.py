3ea39bfe08bed3e002e166b78bdb941f20e48e2d
lo98be <sticazz1>
2018-03-15 19:34:59 +0100

    lool

--- euristico.py
+++ euristico.py
@@ -2,33 +2,47 @@ from data_structure import *
 
 
 def read_file():
-    path = 'first_adventure.in'
-    i_file = open(path, 'r')
-    num_providers, num_services, num_countries, num_projects = list(map(int, i_file.readline().split(' ')))
-    services.append(i_file.readline().split(' '))
-    countries.append(i_file.readline().split(' '))
+    in_file = open('first_adventure.in', 'r')
+    num_providers, num_services, num_countries, num_projects = list(map(int, in_file.readline().split(' ')))
+    print(num_providers, num_services, num_countries, num_projects)
+    services.append(in_file.readline().split(' ', num_services))
+    print(services)
+    countries.append(in_file.readline().split(' ', num_countries))
+    print(countries)
     for _ in range(num_providers):
-        provider_list = i_file.readline().split(' ')
+        provider_list = in_file.readline().split(' ')
+        print(provider_list)
         provider_name = provider_list[0]
         num_regions = int(provider_list[1])
         provider_temp = Provider(provider_name)
         for _ in range(num_regions):
-            region_name = i_file.read()
-            package_number = int(i_file.read())
-            cost = float(i_file.read())
-            service = list(map(int, i_file.readline().split(' ')))
-            latency = list(map(int, i_file.readline().split(' ')))
+            region_name = in_file.read()
+            print(region_name)
+            temp_shit = in_file.read()
+            print(temp_shit)
+            package_number = int(temp_shit)
+            print(package_number)
+            cost = float(in_file.read())
+            print(cost)
+            service = list(map(int, in_file.readline().split(' ')))
+            print(service)
+            latency = list(map(int, in_file.readline().split(' ')))
+            print(latency)
             region_temp = Region(region_name, package_number, cost, service, latency)
             provider_temp.add_region(region_temp)
-
+            print(len(providers))
         providers.append(provider_temp)
-
+    print(providers)
+    print(len(providers))
     for _ in range(num_projects):
-        penalty = int(i_file.read())
-        country = i_file.read()
-        services_project = list(map(int, i_file.readline().split(' ')))
+        penalty = int(in_file.read())
+        country = in_file.read()
+        services_project = list(map(int, in_file.readline().split(' ')))
         project_temp = Project(penalty, country, services_project)
         projects.append(project_temp)
+    print(projects)
+    print(len(projects))
+    in_file.close()
 
 
 def eur_main():
@@ -43,7 +57,7 @@ def eur_main():
             while reg.package_number > n:
                 reg.package_number -= n
                 o_file.write(str(n_p) + " " + str(n_r) + " " + n)
-                num_proj-=1
+                num_proj -= 1
                 if num_proj == 0:
                     o_file.write('\n')
                     return
