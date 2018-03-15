56d2c884debf0e25e0c674e03323d2ac49f9c505
Nik <niky006@gmail.com>
2018-03-15 18:01:03 +0100

    Start working on data structure

--- data_structure.py
+++ data_structure.py
@@ -1,10 +1,27 @@
-"""
------------------
-Provider
------------------
-"""
-
-
 class Provider:
+    """
+    -----------------
+    Provider
+    -----------------
+    """
+
     def __init__(self, name):
         self.name = name
+        self.region = dict()
+
+    def add_region(self, region_name, region):
+        self.region[region_name] = region
+
+
+class Region:
+    """
+    --------
+    Region
+    --------
+    """
+
+    def __init__(self, name: str, package_number: int, cost: float, service: dict):
+        self.name = name
+        self.package_number = package_number
+        self.cost = cost
+        self.service = service
